22dd4c8d0f05f54fa18d1484ff7faeee776359f7
eantipov <egor.antipov@cern.ch>
2021-04-09 21:00:08 +0200

    updated the preparation of skimmed ntuple

--- TMVA/create_skimmed_ntuple.c
+++ TMVA/create_skimmed_ntuple.c
@@ -10,63 +10,67 @@
 ///////////////////
 void create_skimmed_ntuple()
 {
-  // Open the tfile - mtva ouput
-  TFile *tmva_file = new TFile("TMVA400.root");
-  
-  
-  // Open the test tree
-  TTree *test_tree = (TTree*)tmva_file->Get("dataset/TestTree");
-  
-  
-  // Set branches
-  float kbdt_val, weight, topHOF, jet_DL1r_weight;
-  float event_number;
-  kbdt_val = weight = topHOF = jet_DL1r_weight = 0;
-  event_number = 0;
-  
-  test_tree->SetBranchAddress("topHadronOriginFlag", &topHOF);
-  test_tree->SetBranchAddress("weight", &weight);
-  test_tree->SetBranchAddress("KBDT", &kbdt_val);
-  test_tree->SetBranchAddress("NN_jet_DL1r", &jet_DL1r_weight);
-  test_tree->SetBranchAddress("NN_event_number", &event_number);
-  
-  
   // Set some variables for the analysis
   double kbdt_cut = 0.1;
   int n_jets_from_top = 0;
   map<int, map<int, vector<double>> > event_jetn_dl1r_kbdt;
   // { {event_0: {jet_0 - {DL1r, KBDT}}, {jet_1 - {DL1r, KBDT}} ... },
   //   {event_1: {jet_0 - {DL1r, KBDT}}, {jet_1 - {DL1r, KBDT}} ... }, etc }
+
+
+  // Open the tfile - mtva output
+  TFile *tmva_file = new TFile("TMVA400.root");
   
   
-  // Get number of entries
-  Int_t nEntries = test_tree->GetEntries();
-  
-  
-  for (int entry=0; entry<nEntries; entry++) {
+  // Open trees
+  vector<TString> tree_names = {"TestTree", "TrainTree"};
+  for (int tree_count=0; tree_count<tree_names.size(); tree_count++) {
+    TTree *tmva_tree = (TTree*)tmva_file->Get("dataset/" + tree_names[tree_count]);
+    cout << "working with " << tree_names[tree_count] << " tree!" << endl;
     
-    if (entry%1000==0) { cout << "\t" << entry << "\r"; cout.flush(); }
-    test_tree->GetEntry(entry);
+    
+    // Set branches
+    float kbdt_val, weight, topHOF, jet_DL1r_weight;
+    float event_number;
+    kbdt_val = weight = topHOF = jet_DL1r_weight = 0;
+    event_number = 0;
+  
+    tmva_tree->SetBranchAddress("topHadronOriginFlag", &topHOF);
+    tmva_tree->SetBranchAddress("weight", &weight);
+    tmva_tree->SetBranchAddress("KBDT", &kbdt_val);
+    tmva_tree->SetBranchAddress("NN_jet_DL1r", &jet_DL1r_weight);
+    tmva_tree->SetBranchAddress("NN_event_number", &event_number);
+  
+    
+    // Get number of entries
+    Int_t nEntries = tmva_tree->GetEntries();
+  
+  
+    for (int entry=0; entry<nEntries; entry++) {
+      
+      if (entry%1000==0) { cout << "\t" << entry << "\r"; cout.flush(); }
+      tmva_tree->GetEntry(entry);
    
     
-    // Black magic
-    map<int, vector<double> > jetn_dl1r_kbdt;
-    vector<double> dl1r_kbdt = {jet_DL1r_weight, kbdt_val, weight, topHOF};
-    int jetn;
+      // Black magic
+      map<int, vector<double> > jetn_dl1r_kbdt;
+      vector<double> dl1r_kbdt = {jet_DL1r_weight, kbdt_val, weight, topHOF};
+      int jetn;
     
-    map<int, map<int, vector<double>> >::iterator it2 = event_jetn_dl1r_kbdt.find(event_number);
-    if ( it2 != event_jetn_dl1r_kbdt.end() ) {
-      jetn_dl1r_kbdt = it2->second;
-      jetn = jetn_dl1r_kbdt.size();
-      jetn_dl1r_kbdt.insert(make_pair(jetn, dl1r_kbdt));
-      it2->second = jetn_dl1r_kbdt; }
-    else {
-      jetn_dl1r_kbdt.insert(make_pair(0, dl1r_kbdt));
-      event_jetn_dl1r_kbdt.insert(make_pair(event_number, jetn_dl1r_kbdt)); }
+      map<int, map<int, vector<double>> >::iterator it2 = event_jetn_dl1r_kbdt.find(event_number);
+      if ( it2 != event_jetn_dl1r_kbdt.end() ) {
+	jetn_dl1r_kbdt = it2->second;
+	jetn = jetn_dl1r_kbdt.size();
+	jetn_dl1r_kbdt.insert(make_pair(jetn, dl1r_kbdt));
+	it2->second = jetn_dl1r_kbdt; }
+      else {
+	jetn_dl1r_kbdt.insert(make_pair(0, dl1r_kbdt));
+	event_jetn_dl1r_kbdt.insert(make_pair(event_number, jetn_dl1r_kbdt)); }
       
-      
-  } // [entry] - loop over entries
-  
+    } // [entry] - loop over entries
+    
+  } // [tree_count] - loop over trees
+    
   tmva_file->Close();
 
 
@@ -90,10 +94,10 @@ void create_skimmed_ntuple()
 
   // Declare new branches
   tree->Branch("event_number", &event_number_save, "event_number/I");
-  tree->Branch("jet_DL1r", &DL1r_tag_weight_save, "jet_DL1r/F");
-  tree->Branch("KBDT", &kbdt_save, "KBDT/F");
-  tree->Branch("weight", &weight_save, "weight/F");
-  tree->Branch("topHadronOriginFlag", &topHOF_save, "topHadronOriginFlag/F");
+  tree->Branch("jet_DL1r", &DL1r_tag_weight_save);
+  tree->Branch("KBDT", &kbdt_save);
+  tree->Branch("weight", &weight_save);
+  tree->Branch("topHadronOriginFlag", &topHOF_save);
   
 
   // Run a loop over keys - event numbers
